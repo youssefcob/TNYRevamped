333b6151c6347e0d1efd5ce3ffb1e7a0a0859e06
youssefcob <164803162+youssefcob@users.noreply.github.com>
2025-02-05 20:59:33 +0200

    routing test

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,1 @@
+<template><h1>smth</h1></template>
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,7 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('Welcome');
 });
+
+Route::get('/about', function () {
+    return Inertia::render('About');
+});
