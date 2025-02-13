3820ef1fd7ef91e2c7f875d848f8362c8cc15d25
Moaaz Ibrahim <75554068+moaaz-ibrahim@users.noreply.github.com>
2025-02-13 10:15:11 +0200

    Helper functions for structured response

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,4 +5,18 @@ namespace App\Http\Controllers;
 abstract class Controller
 {
     //
+    public function sendResponse($data,$code=200){
+        unset($data['success']);
+        unset($data['code']);
+        $code = $data['code'] ?? $code;
+        $result = ['success' =>true, 'response' =>$data, 'code' =>$code];
+        return response()->json($result,$code);
+    }
+    public function sendError($data,$code=400){
+        unset($data['success']);
+        unset($data['code']);
+        $code = $data['code'] ?? $code;
+        $result = ['success' =>false, 'response' =>$data, 'code' =>$code];
+        return response()->json($result,$code);
+    }
 }
