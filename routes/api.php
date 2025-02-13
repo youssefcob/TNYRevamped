19902b0a806712f371dfcf967c64e945562a5db1
Moaaz Ibrahim <75554068+moaaz-ibrahim@users.noreply.github.com>
2025-02-13 11:26:11 +0200

    Endpoints

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 // use App\Http\Controllers\AdminAtuhController;
 use App\Http\Controllers\AdminAuthController;
+use App\Http\Controllers\ApplicationsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -12,3 +13,8 @@ Route::get('/user', function (Request $request) {
 
 
 Route::post('/login', [AdminAuthController::class, 'login']);
+Route::get('/applications',[ApplicationsController::class , 'getApplications'])->name('get.applications');
+Route::middleware('auth:api')->group(function () {
+    Route::put('/updateApplicationStatus', [ApplicationsController::class , 'updateApplicationStatus'])->name('update.application.status');
+    Route::delete('/deleteApplication', [ApplicationsController::class , 'deleteApplication'])->name('delete.application');
+});
