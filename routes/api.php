d537e61fa5483c239646af5f2acef21f51c52359
Moaaz Ibrahim <75554068+moaaz-ibrahim@users.noreply.github.com>
2025-02-17 13:18:26 +0200

    Adding Positions routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 // use App\Http\Controllers\AdminAtuhController;
 use App\Http\Controllers\AdminAuthController;
 use App\Http\Controllers\ApplicationsController;
+use App\Http\Controllers\PositionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -13,8 +14,16 @@ Route::get('/user', function (Request $request) {
 
 
 Route::post('/login', [AdminAuthController::class, 'login']);
-Route::get('/applications',[ApplicationsController::class , 'getApplications'])->name('get.applications');
 Route::middleware('auth:api')->group(function () {
-    Route::put('/updateApplicationStatus', [ApplicationsController::class , 'updateApplicationStatus'])->name('update.application.status');
-    Route::delete('/deleteApplication', [ApplicationsController::class , 'deleteApplication'])->name('delete.application');
+    //Applications routes
+    Route::get('/applications',[ApplicationsController::class , 'getApplications'])->name('get.applications');
+    //TODO: When delete application make sure to delete the application's files from Cloudinary.
+    Route::put('/applicationStatus', [ApplicationsController::class , 'updateApplicationStatus'])->name('update.application.status');
+    Route::delete('/application', [ApplicationsController::class , 'deleteApplication'])->name('delete.application');
+
+    //Positions routes
+    Route::get('/positions', [PositionController::class , 'getPositions'])->name('get.positions');    
+    Route::put('/positions', [PositionController::class , 'updatePosition'])->name('update.positions');
+    Route::delete('/positions', [PositionController::class , 'deletePosition'])->name('delete.positions');
+    Route::post('/positions', [PositionController::class , 'createPosition'])->name('create.positions');
 });
