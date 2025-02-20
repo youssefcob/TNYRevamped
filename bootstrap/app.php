b3ab7a9d601334790d0d8fe7a5d1e1e92e9c50d8
youssefcob <164803162+youssefcob@users.noreply.github.com>
2025-02-20 21:21:03 +0200

    Appended a middleware to force json responses

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append(\App\Http\Middleware\ForceJsonResponse::class);
+        $middleware->alias([
+
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
