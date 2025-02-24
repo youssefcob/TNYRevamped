50d3ddb2c8885cddcf389478699be67cac5699d7
Moaaz Ibrahim <75554068+moaaz-ibrahim@users.noreply.github.com>
2025-02-24 09:07:32 +0200

    DB Models

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use App\GenericObserverTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Application extends Model
 {
     //
-    use HasFactory;
+    use HasFactory, GenericObserverTrait;
     protected $fillable = ['name', 'email', 'address', 'phone', 'resume', 'position_id', 'status'];
 
 }
