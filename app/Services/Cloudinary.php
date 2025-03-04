8befb71409835a8670d5625c5f9dbc6b273c2719
Moaaz Ibrahim <75554068+moaaz-ibrahim@users.noreply.github.com>
2025-03-04 12:52:03 +0200

    Adding new functions to Cloudinary.php

--- app/Services/Cloudinary.php
+++ app/Services/Cloudinary.php
@@ -22,6 +22,23 @@ class Cloudinary
         return $uploadedFileUrl;
     }
 
+    /**
+     * Uploads an image to Cloudinary and returns the secure URL of the uploaded image and id of the image.
+     *
+     * @return array The secure URL of the uploaded image, public_id on Cloudinary.
+     */
+    public function uploadImageWithId($image): array
+    {
+        $imagePath = $image->getRealPath();
+        $uploadedFile = BaseCloudinary::upload($imagePath);
+        $url = $uploadedFile->getSecurePath();
+        $id = $uploadedFile->getPublicId();
+        return [
+            'file' => $url,
+            'id' => $id
+        ];
+    }
+
 
     /**
      * Deletes an image from Cloudinary.
@@ -57,6 +74,24 @@ class Cloudinary
         $this->deleteImage($oldImagePath);
 
         $uploadedFileUrl = $this->uploadImage($newImage);
+        // Upload the new image
+
+        return $uploadedFileUrl;
+    }
+    /**
+     * Updates an image by deleting the old image and uploading a new one.
+     *
+     * @param string $oldImagePath The path of the old image to be deleted.
+     * @param mixed $newImage The new image to be uploaded.
+     * @return string The URL of the newly uploaded image.
+     */
+
+    public function updateImageWithId($oldImagePath, $newImage)
+    {
+        // Delete the old image
+        $this->deleteImage($oldImagePath);
+
+        $uploadedFileUrl = $this->uploadImageWithId($newImage);
         // Upload the new image
 
         return $uploadedFileUrl;
