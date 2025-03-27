398f717781022e4bf6b5deb68193daf7ed4015b6
youssefcob <164803162+youssefcob@users.noreply.github.com>
2025-03-28 00:54:35 +0200

    Create Services.vue

---
+++ resources/js/Pages/Services.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import NavBar from '@/Components/NavBar/NavBar.vue';
+import Footer from '@/Components/Footer/Foot.vue';
+
+import { Service } from '@/interface/Types';
+import ServicesView from '@/Views/Services.vue';
+
+const props = defineProps({
+    services: {
+        type: Object as () => Service[]
+    },
+});
+
+</script>
+
+<template>
+    <NavBar active="services" />
+    <main>
+        <ServicesView :services="services" />
+    </main>
+    <Footer />
+
+</template>
