0404259f925f3d433b50f907b71218ed609ac4ec
Moaaz Ibrahim <75554068+moaaz-ibrahim@users.noreply.github.com>
2025-03-28 22:32:17 +0200

    adding col 'name'

--- database/migrations/2025_03_13_214159_create_clients_table.php
+++ database/migrations/2025_03_13_214159_create_clients_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
             $table->string('image');
+            $table->string('name');
             $table->timestamps();
         });
     }
