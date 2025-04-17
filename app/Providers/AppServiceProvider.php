d4d266a4b462a47d26a84c77ccf2f828d8fb791a
youssefcob <164803162+youssefcob@users.noreply.github.com>
2025-04-17 13:42:57 +0200

    forcing https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,10 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\URL;
+
+
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if(config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
