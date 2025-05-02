27b594597294a4f4a4d10d0ddffeb8143f924964
Moaaz Ibrahim <75554068+moaaz-ibrahim@users.noreply.github.com>
2025-05-02 09:45:45 +0300

    Text fixing

--- resources/js/Components/AboutUs/AboutUs.vue
+++ resources/js/Components/AboutUs/AboutUs.vue
@@ -8,8 +8,8 @@ import { Link } from '@inertiajs/vue3';
         <h2 class="title">About us</h2>
         <div class="wrapper">
         <div class="text-wrapper">
-            <p>Therapy of New York is a well-recognized leader in physical therapy staffing solutions for medical
-                facilities in New York and New Jersey City. We strive to recruit highly-trained, compassionate medical
+            <p>Therapy of New York is a well-recognized  leader in Medical staffing including Physical, Occupational & Speech therapy for medical
+                facilities in New York and New Jersey. We strive to recruit highly-trained, compassionate medical
                 professionals who possess exceptional skills and a strong work ethic to meet the high standards of
                 healthcare facilities. We’re dedicated to providing the highest-quality medical staffing solutions that
                 are customized to the needs of each client. Taking this into action, over the past years we have placed
