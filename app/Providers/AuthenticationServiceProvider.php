6869b4234fac71b681d46dce9bde854446b657c5
youssefcob <164803162+youssefcob@users.noreply.github.com>
2025-05-28 21:16:12 +0300

    added passport as authentication service provider and added scopes

---
+++ app/Providers/AuthenticationServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
+
+
+class AuthenticationServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+         Passport::tokensCan([
+            'job-seeker' => 'Seek Jobs',
+            'employer' => 'Employ',
+        ]);
+    }
+}
