51e3d756b8adaeb34763626eb1ecd1bd3790b467
Moaaz Ibrahim <75554068+moaaz-ibrahim@users.noreply.github.com>
2025-06-05 15:43:24 +0300

    Applications | Bids tables

---
+++ database/migrations/2025_05_20_083206_create_applications_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('vacancy_id')->constrained('vacancies')->onDelete('cascade');
+            $table->foreignId('job_seeker_id')->constrained('job_seekers')->onDelete('cascade');
+            $table->string('resume')->nullable();
+            $table->string('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('applications');
+    }
+};
