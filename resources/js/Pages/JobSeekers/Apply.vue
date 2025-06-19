f4c44549949dc34d276afeb936a051456a591a91
youssefcob <164803162+youssefcob@users.noreply.github.com>
2025-06-20 02:53:32 +0300

    will be removed eventually

---
+++ resources/js/Pages/JobSeekers/Apply.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import MainOverLay from '@/Components/Overlays/MainOverLay.vue';
+import { Job } from '@/interface/Types';
+import { assignJob } from '@/state/state';
+import Apply from '@/Views/Apply.vue';
+
+const props = defineProps({
+    jobs: {
+        type: Object as () => Job[]
+    },
+    position: {
+        type: String,
+        default: ''
+    },
+});
+if (props.jobs) {
+    assignJob(props.jobs);
+}
+</script>
+
+<template>
+    <MainOverLay>
+        <Apply :position="position" />
+    </MainOverLay>
+</template>
