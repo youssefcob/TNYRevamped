c89810bf2dd22bff9257ae5c87099d20c8b77103
youssefcob <164803162+youssefcob@users.noreply.github.com>
2025-06-20 02:53:37 +0300

    Create JobSeekerProfile.vue

---
+++ resources/js/Pages/JobSeekers/JobSeekerProfile.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import MainOverlay from '@/Components/Overlays/MainOverLay.vue';
+
+
+</script>
+
+<template>
+    <MainOverlay>
+        <div>
+            <h1>Profile</h1>
+        </div>
+    </MainOverlay>
+</template>
+
+
+<style scoped lang="scss">
+</style>
