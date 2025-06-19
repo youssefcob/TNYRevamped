8c81501b081f3943475f47c4e24015b14efd0417
youssefcob <164803162+youssefcob@users.noreply.github.com>
2025-06-20 02:54:52 +0300

    lil svg for file input field

--- resources/js/SharedComponents/FileInputField.vue
+++ resources/js/SharedComponents/FileInputField.vue
@@ -7,14 +7,14 @@ const props = defineProps({
     label: String
 });
 
-const makeid = (length:number)=> {
+const makeid = (length: number) => {
     let result = '';
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
     const charactersLength = characters.length;
     let counter = 0;
     while (counter < length) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      counter += 1;
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
     }
     return result;
 }
@@ -37,93 +37,101 @@ const openFileDialog = () => {
 
 const emit = defineEmits(['input']);
 const HandleFileUpload = (e: Event) => {
-   const  files = ((e.target as HTMLInputElement).files)
-   if (files && files.length > 0) {
+    const files = ((e.target as HTMLInputElement).files)
+    if (files && files.length > 0) {
         const formData = new FormData();
         let file = files[0].name;
-        formData.append('file', files[0]); 
+        formData.append('file', files[0]);
         if (file) {
-        fileName.value = file.split('\\').pop() as string;
-    }
-        emit('input',formData);
+            fileName.value = file.split('\\').pop() as string;
+        }
+        emit('input', formData);
 
     }
 
 }
 </script>
 
-
 <template>
-<div>
-    <input type="file" ref="input" :class="`real-file-input ${id}`" @change="HandleFileUpload">
-        <label :for="label" class="label ">{{ label }}</label>
+    <div>
+        <input type="file" ref="input" :class="`real-file-input ${id}`" @change="HandleFileUpload" />
+        <label :for="label" class="label">{{ label }}</label>
 
-    <div :class="`file-input ${id} big-screen`" @click="openFileDialog">
+        <div :class="`file-input ${id} big-screen`" @click="openFileDialog">
+            <div class="file-placeHolder">
+                <div>
+                    <label>
+                        {{ fileName }}
+                    </label>
+                    <span v-if="required && checkName()" style="color: red">*</span>
+                </div>
+                <div>
+                    <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M21 15.5938V19.5938C21 20.1242 20.7893 20.6329 20.4142 21.008C20.0391 21.383 19.5304 21.5938 19 21.5938H5C4.46957 21.5938 3.96086 21.383 3.58579 21.008C3.21071 20.6329 3 20.1242 3 19.5938V15.5938"
+                            stroke="#135672" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M7 10.5938L12 15.5938L17 10.5938" stroke="#135672" stroke-width="2"
+                            stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M12 15.5938V3.59375" stroke="#135672" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round" />
+                    </svg>
+                </div>
 
-        <div class="file-placeHolder">
-            <label>
-                {{ fileName }}
-            </label>
-            <span v-if="required && checkName()" style="color:red;">*</span>
+            </div>
         </div>
-        
     </div>
-</div>
-
-    
 </template>
 <style scoped lang="scss">
 .real-file-input {
     display: none;
 }
+
 .label {
     margin-left: 0.5rem;
-    margin-bottom:0.5rem;
+    margin-bottom: 0.5rem;
     display: block;
     @extend p;
-    font-size:1rem;
-    font-weight:400;
+    font-size: 1rem;
+    font-weight: 400;
 }
 
 .file-input {
+    height: 4rem;
 
+    display: flex;
+    justify-content: space-between;
+    overflow: hidden;
+    align-items: center;
+    cursor: pointer;
 
+    @extend .input-field;
 
+    // @media screen and (max-width: 800px) {
+    //     display: none;
+    // }
 
-        height: 4rem;
-
+    .file-btn {
+        height: 100%;
+        width: 40%;
+        background-color: $blue;
         display: flex;
-        justify-content: space-between;
-        overflow: hidden;
         align-items: center;
-        cursor: pointer;
-
-        @extend .input-field;
-
-        // @media screen and (max-width: 800px) {
-        //     display: none;
-        // }
-
-        .file-btn {
-            height: 100%;
-            width: 40%;
-            background-color: $blue;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            // @extend .btnfont;
-            pointer-events: none;
-            text-align: center;
-            color: $black;
-            // border-radius: $border-radius;
-
-
-        }
-
-        .file-placeHolder {
-            color: rgba(0, 0, 0, 0.6);
-            font-size: 1rem;
-        }
-
+        justify-content: center;
+        // @extend .btnfont;
+        pointer-events: none;
+        text-align: center;
+        color: $black;
+        // border-radius: $border-radius;
     }
+
+    .file-placeHolder {
+        // background-color: red;
+        width: 100%;
+        color: rgba(0, 0, 0, 0.6);
+        font-size: 1rem;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+}
 </style>
