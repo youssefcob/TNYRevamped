a332fd1cde190586a8bc983f2b0a8265c7e3f5ac
youssefcob <164803162+youssefcob@users.noreply.github.com>
2025-06-20 02:49:24 +0300

    getUser function to check if user is authenticated

--- app/Http/Controllers/UserAuthController.php
+++ app/Http/Controllers/UserAuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Services\AuthService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class UserAuthController extends Controller
 {
@@ -29,5 +31,28 @@ class UserAuthController extends Controller
     public function loginWeb(Request $request){
         $response = $this->service->loginWeb($request);
         return $response;
+    }
+
+    public function getUser(Request $request)
+    {
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+
+        if($user === null) {
+            return response()->json([
+                'success' => false,
+                'message' => 'User not authenticated',
+            ], 401);
+        }
+        if($user->hasRole('employer')) {
+            $user->load('employer');
+        } elseif($user->hasRole('job_seeker')) {
+            $user->load('jobSeeker');
+        }
+        return response()->json([
+            'success' => true,
+            'user' => $user,
+        ]);
+
     }
 }
