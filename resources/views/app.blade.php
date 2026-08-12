40456830c0f78de934b2c78a30db3fe4eff0b24f
youssefcob <164803162+youssefcob@users.noreply.github.com>
2026-08-12 21:21:06 +0300

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,15 +5,27 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="google-site-verification" content="R93HnGhs1Sh1hFIYFhejBjfY6ARszOhCCcNA7mcNrSU" />
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
     @routes
 
     @vite(['resources/js/app.ts',"resources/js/Pages/{$page['component']}.vue"])
     @inertiaHead
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-W6PTFZ2H');</script>
+    <!-- End Google Tag Manager -->
 </head>
 
 <body class="antialiased" style="margin:0;padding:0;">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W6PTFZ2H"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     @inertia
 </body>
 
