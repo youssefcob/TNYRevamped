12bf4dd4f64397516638d88d681d662cff6c5f0b
youssefcob <164803162+youssefcob@users.noreply.github.com>
2026-09-09 19:42:42 +0300

    added a section in solutions

--- resources/js/Pages/Solutions/SolutionDetail.vue
+++ resources/js/Pages/Solutions/SolutionDetail.vue
@@ -7,6 +7,7 @@ import LayoutV2 from '@/Components/HomeV2/LayoutV2.vue';
 import SolutionDetailHero from '@/Components/Solutions/detail/SolutionDetailHero.vue';
 import SolutionOverviewSection from '@/Components/Solutions/detail/SolutionOverviewSection.vue';
 import SolutionRolesSection from '@/Components/Solutions/detail/SolutionRolesSection.vue';
+import SolutionSpotlightSection from '@/Components/Solutions/detail/SolutionSpotlightSection.vue';
 import SolutionFocusSection from '@/Components/Solutions/detail/SolutionFocusSection.vue';
 import SolutionSettingsSection from '@/Components/Solutions/detail/SolutionSettingsSection.vue';
 import SolutionWhySection from '@/Components/Solutions/detail/SolutionWhySection.vue';
@@ -31,6 +32,7 @@ provide('pageContent', (usePage().props.pageContent as PageContentMap) ?? {});
     <SolutionDetailHero :solution="props.solution" :page-key="props.pageKey" />
     <SolutionOverviewSection :solution="props.solution" :page-key="props.pageKey" />
     <SolutionRolesSection v-if="props.solution.roles?.length" :solution="props.solution" :page-key="props.pageKey" />
+    <SolutionSpotlightSection :solution="props.solution" :page-key="props.pageKey" />
     <SolutionFocusSection v-if="props.solution.focus?.length" :solution="props.solution" :page-key="props.pageKey" />
     <SolutionSettingsSection v-if="props.solution.settings?.length" :solution="props.solution" :page-key="props.pageKey" />
     <SolutionWhySection v-if="props.solution.why?.length" :solution="props.solution" :page-key="props.pageKey" />
