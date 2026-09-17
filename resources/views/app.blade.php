ebaa02bbd46f530839cf1ab0587ffd001d29c861
youssefcob <164803162+youssefcob@users.noreply.github.com>
2026-09-17 18:18:53 +0300

    add gtm

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -28,11 +28,24 @@
 
       gtag('config', 'G-E4R86VWSS5');
     </script>
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-N6BW3PBD');</script>
+    <!-- End Google Tag Manager -->
 </head>
 
 <body class="antialiased" style="margin:0;padding:0;">
     <!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W6PTFZ2H"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N6BW3PBD"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
     @inertia
